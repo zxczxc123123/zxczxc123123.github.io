82027a5fa8f974f80f0007f7bb19ac68001d85da
zxczxc123123 <vn508353@gmail.com>
2022-02-23 16:54:25 +0800

    Update copyfile.js

--- scripts/copyfile.js
+++ scripts/copyfile.js
@@ -1,2 +1,6 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+const fs = require('fs'); 
+// destination will be created or overwritten by default.
+fs.copyFile('D:\testfolderA\testA.txt', 'D:\testfolderB\testA.txt', (err) => {
+  if (err) throw err;
+  console.log('File was copied to destination');
+});
